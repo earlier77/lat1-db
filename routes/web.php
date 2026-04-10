be558608880a76b48fc08928a6f0874407a05eb5
dzacky <dzacky7777@gmail.com>
2026-04-10 09:41:11 +0700

    tugas 1

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dzacky-arcaya-rahman-24123374', function () {
-    return view('biodata');
-});
-Route::get('/tes', function () {
-    return "INI TES";
-});
-Route::get('/test123', function () {
-    return "BERHASIL";
+Route::get('/', function(){
+    return view ('welcome');
 });
